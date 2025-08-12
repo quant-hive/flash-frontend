a2bd5bdf41d9f77cbefda17719249fcfea3ad464
Sujal Suhaas <sujalsuhaas2007@gmail.com>
2025-08-12 15:32:35 +0530

    feat: update ThemeProvider to set default theme to dark and disable system theme

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -11,7 +11,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       <CursorProvider>
         <ReduxProvider>
           <AuthProvider>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} forcedTheme="dark">
               <SettingsProvider> {children}</SettingsProvider>
             </ThemeProvider>
           </AuthProvider>
